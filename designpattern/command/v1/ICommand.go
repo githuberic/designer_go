e8b64daace3099edf61f18d6d1f39b6e5d20ae7c
lgq <liugq@weipaitang.com>
2021-10-26 20:04:57 +0800

    upadte command-dp

---
+++ designpattern/command/v1/ICommand.go
@@ -0,0 +1,6 @@
+package v1
+
+// 命令接口
+type ICommand interface {
+	Execute()
+}
---
+++ designpattern/command/v1/OpenTV.go
@@ -0,0 +1,14 @@
+package v1
+
+type OpenTV struct {
+	TV *TV
+}
+
+func NewOpenTV() *OpenTV {
+	commandTV := &OpenTV{}
+	commandTV.TV = new(TV)
+	return commandTV
+}
+func (command *OpenTV) Execute() {
+	command.TV.Open()
+}
