755620d1295816843a56f6252ff410c6c54b391b
lgq <liugq@weipaitang.com>
2021-11-03 17:32:42 +0800

    add distributed system

--- designpattern/visitor/e1/VirusKiller.go
+++ designpattern/visitor/e1/VirusKiller.go
@@ -4,11 +4,11 @@ import "fmt"
 
 type VirusKiller struct{}
 
-func (vk VirusKiller) VisitApartment(a Apartment) {
+func (this *VirusKiller) VisitApartment(a Apartment) {
 	fmt.Printf("Kill virus for Apartment %s\n", a.Name)
 }
-func (vk VirusKiller) VisitBuilding(b Building) {
+func (this *VirusKiller) VisitBuilding(b Building) {
 	for e := b.Apartments.Front(); e != nil; e = e.Next() {
-		vk.VisitApartment(e.Value.(Apartment))
+		this.VisitApartment(e.Value.(Apartment))
 	}
 }
