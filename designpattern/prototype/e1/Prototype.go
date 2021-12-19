15ea6eab8d702fb407fee06fc17b61801c6ef299
lgq <liugq@weipaitang.com>
2021-12-19 20:52:23 +0800

    add prototype-e2

--- designpattern/prototype/e1/Prototype.go
+++ designpattern/prototype/e1/Prototype.go
@@ -1,4 +1,4 @@
-package prototype
+package e1
 
 import (
 	"bytes"
--- designpattern/prototype/e1/prototype_v1_test.go
+++ designpattern/prototype/e1/prototype_v1_test.go
@@ -1,11 +1,11 @@
-package prototype
+package e1
 
 import (
 	"fmt"
 	"testing"
 )
 
-func TestVerifyV1(t *testing.T)  {
+func TestVerifyV1(t *testing.T) {
 	ComputerStart4()
 }
 
@@ -19,18 +19,18 @@ func ComputerStart1() {
 	}
 
 	//浅拷贝
-	Pc2:=Pc1
+	Pc2 := Pc1
 	fmt.Printf("PcInfo Pc1:%v, Pc2:%v\n", Pc1, Pc2)
 	//修改切片内容以及map信息影响Pc1
-	Pc2.SystemName ="MacOs"
-	Pc2.UseNumber =100
-	Pc2.Memory.Count =2
-	Pc2.Memory.MemorySize[0]=8
-	Pc2.Memory.MemorySize[1]=8
-	Pc2.Memory.MemorySize[2]=0
-	Pc2.Memory.MemorySize[3]=0
-	Pc2.Fan["left"]=FanSpeed{2000}
-	Pc2.Fan["right"]=FanSpeed{1500}
+	Pc2.SystemName = "MacOs"
+	Pc2.UseNumber = 100
+	Pc2.Memory.Count = 2
+	Pc2.Memory.MemorySize[0] = 8
+	Pc2.Memory.MemorySize[1] = 8
+	Pc2.Memory.MemorySize[2] = 0
+	Pc2.Memory.MemorySize[3] = 0
+	Pc2.Fan["left"] = FanSpeed{2000}
+	Pc2.Fan["right"] = FanSpeed{1500}
 	fmt.Printf("PcInfo Pc1:%v, Pc2:%v\n", Pc1, Pc2)
 }
 
@@ -44,15 +44,15 @@ func ComputerStart2() {
 	}
 
 	//浅拷贝
-	Pc2:=Pc1
+	Pc2 := Pc1
 	fmt.Printf("PcInfo Pc1:%v, Pc2:%v\n", Pc1, Pc2)
 
 	//切片以及map新空间互不影响
-	Pc2.SystemName ="MacOs"
-	Pc2.UseNumber =100
-	Pc2.Memory =Memory{Count: 2, MemorySize: []int{8, 8}}
-	Pc2.Fan =map[string]FanSpeed{"left": {2000}, "right": {1500}}
-	Pc2.Money =Money{1000.45}
+	Pc2.SystemName = "MacOs"
+	Pc2.UseNumber = 100
+	Pc2.Memory = Memory{Count: 2, MemorySize: []int{8, 8}}
+	Pc2.Fan = map[string]FanSpeed{"left": {2000}, "right": {1500}}
+	Pc2.Money = Money{1000.45}
 	fmt.Printf("PcInfo Pc1:%v, Pc2:%v\n", Pc1, Pc2)
 }
 
@@ -66,7 +66,7 @@ func ComputerStart3() {
 	}
 
 	//浅拷贝
-	Pc2:=Pc1
+	Pc2 := Pc1
 	fmt.Printf("PcInfo Pc1:%v, Pc2:%v\n", Pc1, Pc2)
 
 	ModifyCat(Pc2)
@@ -83,8 +83,8 @@ func ComputerStart4() {
 	}
 
 	//深拷贝
-	Pc2:= new(Computer)
-	if err:= deepCopy(Pc2,Pc1);err!=nil{
+	Pc2 := new(Computer)
+	if err := deepCopy(Pc2, Pc1); err != nil {
 		panic(err.Error())
 	}
 	fmt.Printf("PcInfo Pc1:%v, Pc2:%v\n", Pc1, Pc2)
@@ -95,14 +95,14 @@ func ComputerStart4() {
 
 func ModifyCat(pc Computer) {
 	fmt.Printf("PcInfo Pc1:%v\n", pc)
-	pc.SystemName ="MacOs"
-	pc.UseNumber =100
-	pc.Memory.Count =2
-	pc.Memory.MemorySize[0]=8
-	pc.Memory.MemorySize[1]=8
-	pc.Memory.MemorySize[2]=0
-	pc.Memory.MemorySize[3]=0
-	pc.Fan["left"]=FanSpeed{2000}
-	pc.Fan["right"]=FanSpeed{1500}
+	pc.SystemName = "MacOs"
+	pc.UseNumber = 100
+	pc.Memory.Count = 2
+	pc.Memory.MemorySize[0] = 8
+	pc.Memory.MemorySize[1] = 8
+	pc.Memory.MemorySize[2] = 0
+	pc.Memory.MemorySize[3] = 0
+	pc.Fan["left"] = FanSpeed{2000}
+	pc.Fan["right"] = FanSpeed{1500}
 	fmt.Printf("PcInfo Pc1:%v\n", pc)
 }
